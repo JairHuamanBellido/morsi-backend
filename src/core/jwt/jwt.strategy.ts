b20c974ea8ea654066d6680a20c4eb30ee5e30fa
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-08-07 11:59:54 -0700

    Correciòn de jwt en pruebas unitarias y timeout

--- src/core/jwt/jwt.strategy.ts
+++ src/core/jwt/jwt.strategy.ts
@@ -1,14 +1,14 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
-
+import { jwtKey } from "../../config/key.jwt";
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor() {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: process.env.KEY_JWT,
+            secretOrKey: jwtKey.secret,
         });
     }
 
