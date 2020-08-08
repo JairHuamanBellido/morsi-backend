06ec3a40b1f1e691355ee7797633d85c549635e0
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2020-08-08 07:56:59 -0700

    Validacion de campos para creación de empleados

--- src/modules/employees/dto/createEmployee.dto.ts
+++ src/modules/employees/dto/createEmployee.dto.ts
@@ -1,13 +1,48 @@
+import { IsNotEmpty, IsString, IsNumber } from "class-validator";
+
 export class CreateEmployee {
-    idRestaurant: string;
-    dni: string;
-    name: string;
-    lastname: string;
-    age: number;
-    address: string;
-    city: string;
-    district: string;
-    nationality: string;
-    phone: string;
-    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly idRestaurant: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly dni: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly name: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly lastname: string;
+
+    @IsNotEmpty()
+    @IsNumber()
+    readonly age: number;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly address: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly city: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly district: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly nationality: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly phone: string;
+
+    @IsNotEmpty()
+    @IsString()
+    readonly email: string;
 }
